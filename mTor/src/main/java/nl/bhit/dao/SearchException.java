e4995c725b1177a77b203c133b3ac8c9d6812c6e
Vyara <vyara.wf@gmail.com>
2013-01-16 14:41:15 +0200

    Initial commit of mTor

---
+++ mTor/src/main/java/nl/bhit/dao/SearchException.java
@@ -0,0 +1,13 @@
+package nl.bhit.dao;
+
+/**
+ *
+ * @author jgarcia
+ */
+public class SearchException extends RuntimeException {
+
+    public SearchException(Throwable ex) {
+        super(ex);
+    }
+
+}
