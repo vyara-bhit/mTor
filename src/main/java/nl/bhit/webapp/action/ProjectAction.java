37a9ccb3265f54fe5b0b8a9a501515a3d9e68230
Tibor Strausz <tibor@strausz.nl>
2013-01-18 17:14:55 +0100

    adding the company list to the action

--- src/main/java/nl/bhit/webapp/action/ProjectAction.java
+++ src/main/java/nl/bhit/webapp/action/ProjectAction.java
@@ -1,104 +1,116 @@
 package nl.bhit.webapp.action;
 
-import com.opensymphony.xwork2.Preparable;
-import nl.bhit.service.GenericManager;
-import nl.bhit.dao.SearchException;
-import nl.bhit.model.Project;
-import nl.bhit.webapp.action.BaseAction;
-
 import java.util.List;
 
+import nl.bhit.dao.SearchException;
+import nl.bhit.model.Company;
+import nl.bhit.model.Project;
+import nl.bhit.service.GenericManager;
+
+import com.opensymphony.xwork2.Preparable;
+
 public class ProjectAction extends BaseAction implements Preparable {
-    private GenericManager<Project, Long> projectManager;
-    private List projects;
-    private Project project;
-    private Long id;
-    private String query;
+	private GenericManager<Project, Long> projectManager;
+	private GenericManager<Company, Long> companyManager;
+	private List projects;
+	private List companies;
+	private Project project;
+	private Long id;
+	private String query;
 
-    public void setProjectManager(GenericManager<Project, Long> projectManager) {
-        this.projectManager = projectManager;
-    }
+	public void setProjectManager(GenericManager<Project, Long> projectManager) {
+		this.projectManager = projectManager;
+	}
 
-    public List getProjects() {
-        return projects;
-    }
+	public List getProjects() {
+		return projects;
+	}
 
-    /**
-     * Grab the entity from the database before populating with request parameters
-     */
-    public void prepare() {
-        if (getRequest().getMethod().equalsIgnoreCase("post")) {
-            // prevent failures on new
-            String projectId = getRequest().getParameter("project.id");
-            if (projectId != null && !projectId.equals("")) {
-                project = projectManager.get(new Long(projectId));
-            }
-        }
-    }
+	public void setCompanyManager(GenericManager<Company, Long> companyManager) {
+		this.companyManager = companyManager;
+	}
 
-    public void setQ(String q) {
-        this.query = q;
-    }
+	/**
+	 * Grab the entity from the database before populating with request parameters
+	 */
+	@Override
+	public void prepare() {
+		if (getRequest().getMethod().equalsIgnoreCase("post")) {
+			// prevent failures on new
+			String projectId = getRequest().getParameter("project.id");
+			if (projectId != null && !projectId.equals("")) {
+				project = projectManager.get(new Long(projectId));
+			}
+		}
+	}
 
-    public String list() {
-        try {
-            projects = projectManager.search(query, Project.class);
-        } catch (SearchException se) {
-            addActionError(se.getMessage());
-            projects = projectManager.getAll();
-        }
-        return SUCCESS;
-    }
+	public void setQ(String q) {
+		this.query = q;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String list() {
+		try {
+			projects = projectManager.search(query, Project.class);
+		} catch (SearchException se) {
+			addActionError(se.getMessage());
+			projects = projectManager.getAll();
+		}
+		return SUCCESS;
+	}
 
-    public Project getProject() {
-        return project;
-    }
+	public List getCompanyList() {
+		return companyManager.search(query, Company.class);
+	}
 
-    public void setProject(Project project) {
-        this.project = project;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String delete() {
-        projectManager.remove(project.getId());
-        saveMessage(getText("project.deleted"));
+	public Project getProject() {
+		return project;
+	}
 
-        return SUCCESS;
-    }
+	public void setProject(Project project) {
+		this.project = project;
+	}
 
-    public String edit() {
-        if (id != null) {
-            project = projectManager.get(id);
-        } else {
-            project = new Project();
-        }
+	public String delete() {
+		projectManager.remove(project.getId());
+		saveMessage(getText("project.deleted"));
 
-        return SUCCESS;
-    }
+		return SUCCESS;
+	}
 
-    public String save() throws Exception {
-        if (cancel != null) {
-            return "cancel";
-        }
+	public String edit() {
+		if (id != null) {
+			project = projectManager.get(id);
+		} else {
+			project = new Project();
+		}
 
-        if (delete != null) {
-            return delete();
-        }
+		return SUCCESS;
+	}
 
-        boolean isNew = (project.getId() == null);
+	public String save() throws Exception {
+		if (cancel != null) {
+			return "cancel";
+		}
 
-        projectManager.save(project);
+		if (delete != null) {
+			return delete();
+		}
 
-        String key = (isNew) ? "project.added" : "project.updated";
-        saveMessage(getText(key));
+		boolean isNew = (project.getId() == null);
 
-        if (!isNew) {
-            return INPUT;
-        } else {
-            return SUCCESS;
-        }
-    }
+		projectManager.save(project);
+
+		String key = (isNew) ? "project.added" : "project.updated";
+		saveMessage(getText(key));
+
+		if (!isNew) {
+			return INPUT;
+		} else {
+			return SUCCESS;
+		}
+	}
 }
