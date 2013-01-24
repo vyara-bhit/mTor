4f6816ca4cde5d5368f4453ce6b5df444ea618ab
Vyara <vyara.wf@gmail.com>
2013-01-24 15:38:03 +0200

    Fix the bug with project null

--- src/main/java/nl/bhit/webapp/action/MessageAction.java
+++ src/main/java/nl/bhit/webapp/action/MessageAction.java
@@ -9,6 +9,8 @@ import nl.bhit.model.Status;
 import nl.bhit.service.GenericManager;
 import nl.bhit.service.MessageManager;
 
+
+
 import com.opensymphony.xwork2.Preparable;
 
 public class MessageAction extends BaseAction implements Preparable {
@@ -21,6 +23,8 @@ public class MessageAction extends BaseAction implements Preparable {
     private Long id;
     private String query;
 
+
+    
     public void setMessageManager(MessageManager messageManager) {
         this.messageManager = messageManager;
     }
@@ -97,6 +101,8 @@ public class MessageAction extends BaseAction implements Preparable {
         return SUCCESS;
     }
 
+
+    
     public String save() throws Exception {
         if (cancel != null) {
             return "cancel";
@@ -109,6 +115,7 @@ public class MessageAction extends BaseAction implements Preparable {
         boolean isNew = (message.getId() == null);
 
         messageManager.save(message);
+              
 
         String key = (isNew) ? "message.added" : "message.updated";
         saveMessage(getText(key));
@@ -119,4 +126,6 @@ public class MessageAction extends BaseAction implements Preparable {
             return SUCCESS;
         }
     }
+    
+    
 }
