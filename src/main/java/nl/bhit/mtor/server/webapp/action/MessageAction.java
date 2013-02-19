5ff8a13c78cb6bfd10d7749efe848b6b36dd1907
Tibor Strausz <tibor@strausz.nl>
2013-02-19 19:35:14 +0100

    repackaged webapp into the server package

---
+++ src/main/java/nl/bhit/mtor/server/webapp/action/MessageAction.java
@@ -0,0 +1,169 @@
+package nl.bhit.mtor.server.webapp.action;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import nl.bhit.mtor.dao.SearchException;
+import nl.bhit.mtor.model.MTorMessage;
+import nl.bhit.mtor.model.Project;
+import nl.bhit.mtor.model.Status;
+import nl.bhit.mtor.model.User;
+import nl.bhit.mtor.server.webapp.util.UserManagementUtils;
+import nl.bhit.mtor.service.GenericManager;
+import nl.bhit.mtor.service.MessageManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.opensymphony.xwork2.Preparable;
+
+@Component
+public class MessageAction extends BaseAction implements Preparable {
+	@Autowired
+	private MessageManager messageManager;
+	@Autowired
+	private GenericManager<Project, Long> projectManager;
+	private List mTorMessages;
+	private List projects;
+	private List status;
+	private MTorMessage message;
+	private Long id;
+	private String query;
+
+	public void setMessageManager(MessageManager messageManager) {
+		this.messageManager = messageManager;
+	}
+
+	public void setProjectManager(GenericManager<Project, Long> projectManager) {
+		this.projectManager = projectManager;
+	}
+
+	public List getMTorMessages() {
+		return mTorMessages;
+	}
+
+	/**
+	 * Grab the entity from the database before populating with request parameters
+	 */
+	@Override
+	public void prepare() {
+		if (getRequest().getMethod().equalsIgnoreCase("post")) {
+			// prevent failures on new
+			String messageId = getRequest().getParameter("message.id");
+			if (messageId != null && !messageId.equals("")) {
+				message = messageManager.get(new Long(messageId));
+			}
+		}
+	}
+
+	public void setQ(String q) {
+		this.query = q;
+	}
+
+	public String list() {
+		try {
+			// messages = messageManager.search(query, Message.class);
+			mTorMessages = new ArrayList();
+			List<MTorMessage> tempMessages = messageManager.search(query, MTorMessage.class);
+			List<Project> tempProjects = getProjectCompanyList();
+			for (MTorMessage tempMessage : tempMessages) {
+				String messageProjectName = tempMessage.getProject().getName();
+				for (Project tempProject : tempProjects) {
+					if (tempProject.getName().equalsIgnoreCase(messageProjectName)) {
+						mTorMessages.add(tempMessage);
+					}
+				}
+			}
+			Collection messagesNew = new LinkedHashSet(mTorMessages);
+			mTorMessages = new ArrayList(messagesNew);
+		} catch (SearchException se) {
+			addActionError(se.getMessage());
+			mTorMessages = messageManager.getAllDistinct();
+		}
+		return SUCCESS;
+	}
+
+	public List getProjectCompanyList() {
+		List<Project> tempProjects = projectManager.getAllDistinct();
+		String loggedInUser = UserManagementUtils.getAuthenticatedUser().getFullName();
+		projects = new ArrayList();
+		for (Project tempProject : tempProjects) {
+			Set<User> projectUsers = tempProject.getUsers();
+			for (User projectUser : projectUsers) {
+				if (projectUser.getFullName().equalsIgnoreCase(loggedInUser)) {
+					projects.add(tempProject);
+				}
+			}
+		}
+		return projects;
+	}
+
+	public List getStatusList() {
+		return Status.getAsList();
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public MTorMessage getMessage() {
+		return message;
+	}
+
+	public void setMessage(MTorMessage message) {
+		this.message = message;
+	}
+
+	public String delete() {
+		messageManager.remove(message.getId());
+		saveMessage(getText("message.deleted"));
+
+		return SUCCESS;
+	}
+
+	public String edit() {
+		if (id != null) {
+			message = messageManager.get(id);
+		} else {
+			message = new MTorMessage();
+		}
+
+		return SUCCESS;
+	}
+
+	public String save() throws Exception {
+		if (cancel != null) {
+			return "cancel";
+		}
+
+		if (delete != null) {
+			return delete();
+		}
+
+		boolean isNew = (message.getId() == null);
+
+		messageManager.save(message);
+
+		String key = (isNew) ? "message.added" : "message.updated";
+		saveMessage(getText(key));
+
+		if (!isNew) {
+			return INPUT;
+		}
+		return SUCCESS;
+	}
+
+	public String resolve() {
+		List<MTorMessage> mTorMessagesList = new ArrayList();
+		mTorMessagesList = messageManager.getMessagesWithTimestamp(message);
+
+		for (MTorMessage tempMessage : mTorMessagesList) {
+			tempMessage.setResolved(true);
+			messageManager.save(tempMessage);
+		}
+		return SUCCESS;
+	}
+}
---
+++ src/test/java/nl/bhit/mtor/server/webapp/action/CompanyActionTest.java
@@ -0,0 +1,96 @@
+package nl.bhit.mtor.server.webapp.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.ServletActionContext;
+import nl.bhit.mtor.model.Company;
+import nl.bhit.mtor.server.webapp.action.BaseActionTestCase;
+import nl.bhit.mtor.server.webapp.action.CompanyAction;
+import nl.bhit.mtor.service.GenericManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CompanyActionTest extends BaseActionTestCase {
+    private CompanyAction action;
+
+    @Before
+    public void onSetUp() {
+        super.onSetUp();
+
+        action = new CompanyAction();
+        GenericManager companyManager = (GenericManager) applicationContext.getBean("companyManager");
+        action.setCompanyManager(companyManager);
+
+        // add a test company to the database
+        Company company = new Company();
+
+        // enter all required fields
+        company.setName("" + Math.random());
+
+        companyManager.save(company);
+    }
+
+    @Test
+    public void testGetAllCompanies() throws Exception {
+        assertEquals(action.list(), ActionSupport.SUCCESS);
+        assertTrue(action.getCompanies().size() >= 1);
+    }
+
+    @Test
+    public void testSearch() throws Exception {
+        // regenerate indexes
+        GenericManager<Company, Long> companyManager = (GenericManager<Company, Long>) applicationContext.getBean("companyManager");
+        companyManager.reindex();
+
+        action.setQ("*");
+        assertEquals(action.list(), ActionSupport.SUCCESS);
+        assertEquals(4, action.getCompanies().size());
+    }
+
+    @Test
+    public void testEdit() throws Exception {
+        log.debug("testing edit...");
+        action.setId(-1L);
+        assertNull(action.getCompany());
+        assertEquals("success", action.edit());
+        assertNotNull(action.getCompany());
+        assertFalse(action.hasActionErrors());
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        ServletActionContext.setRequest(request);
+        action.setId(-1L);
+        assertEquals("success", action.edit());
+        assertNotNull(action.getCompany());
+
+        Company company = action.getCompany();
+        // update required fields
+        company.setName("FnJvUxAjMbBtVqGdDvLcGbDnUlUcPpGrYbIdCeFmAeFeOiEoQbJhCdPzFwBnJlMcHzPiMoHfRmVoJiCiJdGrKzPoCgCgVlWaLqXcRcPdSlQjXpMsJbMkLwLdSpMhNmWvRvQkVdLhRsTdPqJaQuJlNuZtJvBmNtJoIkTnTgGmKrXmByRyOiWmVxAtJiDrKxDfUiCnYvXgQlFdLsFePrKvZbUiXfNxImXsTbZtIoSvKwUmOwJfGpBrPoBbMdCdUkD");
+
+        action.setCompany(company);
+
+        assertEquals("input", action.save());
+        assertFalse(action.hasActionErrors());
+        assertFalse(action.hasFieldErrors());
+        assertNotNull(request.getSession().getAttribute("messages"));
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        ServletActionContext.setRequest(request);
+        action.setDelete("");
+        Company company = new Company();
+        company.setId(-2L);
+        action.setCompany(company);
+        assertEquals("success", action.delete());
+        assertNotNull(request.getSession().getAttribute("messages"));
+    }
+}
