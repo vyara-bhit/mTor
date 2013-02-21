51f65a9def9523cb7d568a2b515daf9adafcf2a6
Tibor Strausz <tibor@strausz.nl>
2013-02-21 11:16:51 +0100

    expanding test for messageRetrival

--- src/test/java/nl/bhit/mtor/dao/MessageDaoTest.java
+++ src/test/java/nl/bhit/mtor/dao/MessageDaoTest.java
@@ -6,9 +6,9 @@ import static org.junit.Assert.assertNotNull;
 import java.util.Date;
 import java.util.List;
 
-import nl.bhit.mtor.dao.MessageDao;
 import nl.bhit.mtor.model.MTorMessage;
 
+import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -45,12 +45,24 @@ public class MessageDaoTest extends BaseDaoTestCase {
 	@Test
 	public void testGetMessagesWithTimestamp() {
 		log.debug("starting testGetMessagesWithTimestamp...");
-		MTorMessage message = new MTorMessage();
-		message.setTimestamp(new Date());
+		MTorMessage message = createMessage(new Date());
 		List<MTorMessage> messages = messageDao.getMessagesWithTimestamp(message);
-
 		assertEquals(3, messages.size());
 
+		messageDao.save(createMessage(DateUtils.addDays(new Date(), 1)));
+		messages = messageDao.getMessagesWithTimestamp(message);
+		assertEquals("message added which is newer", 3, messages.size());
+
+		messageDao.save(createMessage(DateUtils.addDays(new Date(), -1)));
+		messages = messageDao.getMessagesWithTimestamp(message);
+		assertEquals("older message added", 4, messages.size());
+	}
+
+	protected MTorMessage createMessage(Date date) {
+		MTorMessage message = new MTorMessage();
+		message.setTimestamp(date);
+		message.setContent("test message from unit test MessageDaoTest");
+		return message;
 	}
 
 }
